c908d8cd5d3a4a1a554eabf002fabc64662a6d6e
Mall0-w <kylelewis@rogers.com>
2024-06-28 09:16:55 -0400

    got a super basic minimal api setup without db

--- portfolio-backend/Program.cs
+++ portfolio-backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,39 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var ProjectList = new List<Project>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/", () => {
+    return "Hello World!";
+});
+
+app.MapPost("/api/projects", (Project p) => {
+    ProjectList.Add(p);
+    return TypedResults.Created("/api/projects/{id}", p);
+});
+
+app.MapGet("/api/projects/{id}", Results<Ok<Project>, NotFound>(long id) => {
+    Project? ToFind = ProjectList.SingleOrDefault(p => p.id == id);
+    return ToFind is null ? TypedResults.NotFound() : TypedResults.Ok(ToFind);
+});
+
+app.MapGet("/api/projects", () => {
+    return TypedResults.Ok(ProjectList);
+});
+
+app.MapDelete("/api/projects/{id}", (long id)=>{
+    ProjectList.RemoveAll(p => p.id == id);
+    return TypedResults.NoContent();
+});
+
+app.MapPatch("/api/projects/{id}", Results<Ok<Project>, NotFound>(Project proj) => {
+    var index = ProjectList.FindIndex(p => p.id == proj.id);
+    if(index == -1)
+        return TypedResults.NotFound();
+    ProjectList[index] = proj;
+    return TypedResults.Ok(proj);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record Project(long id, string name, string desc, DateOnly? dateDone);
