fd70d66c6b2a1a7b548be4a251ee777695a5f3b3
Mall0-w <kylelewis@rogers.com>
2024-06-29 18:23:24 -0400

    simplified code for pages and refs

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,48 +1,41 @@
 import { Box, FormControlLabel, Switch, Paper, Slide, Typography } from "@mui/material";
 import BoilerPlate from "./Boilerplate.js";
-import { useRef, useState } from "react";
+import { createRef, useRef, useState } from "react";
 import Home from "./Pages/Home.js";
 import Contact from "./Pages/Contact.js";
 import Projects from "./Pages/Projects.js";
 import About from "./Pages/About.js";
 
 function App(props) {
+  const pages = ['home', 'projects', 'about', "contact"];
+  // const homeRef = useRef(null);
+  // const projectRef = useRef(null);
+  // const aboutRef = useRef(null);
+  // const contactRef = useRef(null);
 
-  const homeRef = useRef(null);
-  const projectRef = useRef(null);
-  const aboutRef = useRef(null);
-  const contactRef = useRef(null);
+  //using some whacky functionaly programming to get an object of key/ref pairs
+  const sectionRefs = useRef(
+    pages.reduce((acc, section) => {
+      acc[section] = createRef();
+      return acc;
+    }, {})
+  )
 
   function navToRef(page){
-    let refToScroll = null
-    switch(page){
-      case "home":
-        refToScroll = homeRef
-        break;
-      case "projects":
-        refToScroll = projectRef
-        break;
-      case "about":
-        refToScroll = aboutRef
-        break;
-      case "contact":
-        refToScroll = contactRef
-        break;
-      default:
-        console.log(`no scroll ref for ${page}`)
-        break;
-    }
-    console.log("page", page)
+    //if ref exists, scroll to corresponding page
+    if(!sectionRefs.current)
+      return
+    let refToScroll = sectionRefs.current[page]
     if(refToScroll && refToScroll.current)
       refToScroll.current.scrollIntoView({ behavior: 'smooth' })
   }
 
     return (
       <BoilerPlate updateTab={(v) => navToRef(v)}>
-        <Home ref={homeRef}/>
-        <Projects ref={projectRef}/>
-        <About ref={aboutRef}/>
-        <Contact ref={contactRef}/>
+        <Home ref={sectionRefs.current['home']}/>
+        <Projects ref={sectionRefs.current['projects']}/>
+        <About ref={sectionRefs.current['about']}/>
+        <Contact ref={sectionRefs.current['contact']}/>
       </BoilerPlate>
     );
 }
