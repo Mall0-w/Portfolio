dcfbfdd197b57402c17a4234b2145a18e92de4fd
Mall0-w <kylelewis@rogers.com>
2024-07-06 09:41:31 -0400

    fixed text not rendering breaks

--- portfolio-frontend/src/Pages/About.js
+++ portfolio-frontend/src/Pages/About.js
@@ -2,7 +2,7 @@ import { Box, Typography, Grid, Slide } from "@mui/material"
 import { forwardRef } from "react"
 
 //can't just extend the string with a \ since the typewritter lags out then ;-;
-const schpeel = "Hello I'm Kyle <br/> I graduated University of Toronto in June of 2024.  I'm super " +
+const schpeel = "Hello I'm Kyle <br/><br/> I graduated University of Toronto in June of 2024.  I'm super " +
                 "passionate about software development and building things.  It started back in when I was " +
                 "in high school; I took a computer science class and loved how every challenge and program felt like a puzzle to be solved. " +
                 "After that, I was convinced to join the school's robotics team, feel in love with software even more, and before I knew it I was studying it in University. " +
@@ -15,19 +15,19 @@ const schpeel = "Hello I'm Kyle <br/> I graduated University of Toronto in June 
                 "Below you can find some examples of my work.  Don't be afraid to reach out if any of it interests you!"
 
 
-const About = forwardRef((props, ref) => {
+const About = forwardRef(({loaded}, ref) => {
     
     return (
         <Grid container id="about" ref={ref} spacing={2}
         sx={{width:"100%", display:"flex", marginBottom:'5%', flexDirection:'row-reverse', justifyContent:'center', alignItems:'center'}}>
             <Grid item container xs={12} sm={12} md={12} lg={3} sx={{overflow:"hidden", justifyContent:'center', alignItems:'center'}}>
-            <Slide direction="left" in={props.loaded} mountOnEnter unmountOnExit>
+            <Slide direction="left" in={loaded} mountOnEnter unmountOnExit sx={{maxWidth: '100%', maxHeight: '100%', justifyContent:'center', alignItems:'center'}}>
                 <img style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'contain'}} src={require('../assets/images/profile-cropped.png')}/>
             </Slide>
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={9}>
             <Typography color="primary" fontSize={25}>
-                {schpeel}
+                <Box dangerouslySetInnerHTML={{ __html: schpeel }}/>
             </Typography>
             </Grid>
             
