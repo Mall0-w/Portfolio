9be742a0d47b87796dc1dfbb0096346a71900e8b
Mall0-w <kylelewis@rogers.com>
2024-07-05 20:24:20 -0400

    made colours more accessible

--- portfolio-frontend/src/Constants/Colours.js
+++ portfolio-frontend/src/Constants/Colours.js
@@ -1,7 +1,7 @@
 import { green, purple, yellow } from '@mui/material/colors';
 export const Colors= {
     main : {
-        primary: "#26872D",
+        primary: "#30AB38",
         secondary : yellow[800],
         dark: green[900]
     }
